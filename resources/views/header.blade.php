fd6e502b8fcd29b2615cbbd4636c0a020205e5b4
FTyumin <feodor.tjumin.28@gmail.com>
2025-09-22 17:18:34 +0300

    design edits, search bar:

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,22 +1,33 @@
 <header class="flex flex-wrap lg:justify-start lg:flex-nowrap w-full py-5 px-8 z-50 relative border-b border-gray-200">
- <nav class="relative w-full flex justify-around items-center px-4 sm:px-8 lg:px-28 gap-6">    
+ <nav class="relative w-full flex justify-between items-center px-4 sm:px-8 lg:px-28 gap-6">    
   
   <div>
     <span>LOGO</span>
-    <button>menu</button>
+    <button>Menu</button>
   </div>
 
+  <div class="flex-1 max-w-2xl">
+    <form class="">   
+      @csrf
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Movies, Tv Shows, Actors" required />
+            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        </div>
+    </form>
+  </div>
 
   <div class="flex gap-6">
    <div>Movies</div>
    <div>Tv shows</div>
    <div> FYP</div>
-   <div>smth else</div>
+   <div>Sign in</div>
   </div>
-
-    
 
  </nav>
 </header>
-
-
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,16 +29,15 @@
     </div>
   </div>
 
-  
-
 </section>
 
-<div class="mt-10 mx-10">
+<!-- Trending Movies -->
+<div class="my-16 mx-10 sm:px-8 lg:px-28">
     <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Biggest movies <span class="text-blue-600 dark:text-blue-500">Right Now</span></h1>
 
-    <div class="grid grid-cols-3 gap-x-3 gap-y-10">
-        @for ($i = 0; $i < 6; $i++)
-            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <div class="grid grid-cols-4 gap-x-16 gap-y-10">
+        @for ($i = 0; $i < 4; $i++)
+            <div class="w-[24rem] bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
                     <img class="aspect-[3/2] w-full rounded-t-lg object-cover" src="{{ asset('images/cinema.webp') }}" alt="Movie poster placeholder" />
                 </a>
@@ -61,9 +60,31 @@
             </div>   
         @endfor
     </div>
-
 </div>
 
+<!-- Genres -->
+<div class="my-16 mx-10 sm:px-8 lg:px-28">
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Genres</h1>
 
+    <div class="grid grid-cols-4 gap-x-16 gap-y-10">
+        @for ($i = 0; $i < 4; $i++)
+            <div class="w-[24rem] bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Genre Name
+                        </h5>
+                    </a>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus-visible:ring-blue-800">
+                        View Movies
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>   
+        @endfor
+    </div>
+</div>
 
 @endsection
