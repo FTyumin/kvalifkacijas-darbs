b11fccfd3e5621fcae3657718d50dba2ca3e555d
FTyumin <feodor.tjumin.28@gmail.com>
2025-09-23 15:41:37 +0300

    movie,actor, genre migrations

---
+++ database/migrations/2025_09_23_080122_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->year('year'); 
+            $table->text('description');
+            $table->integer('duration')->nullable(); // Runtime in minutes
+            $table->decimal('rating', 3, 1)->nullable(); 
+            $table->string('poster_url')->nullable();
+            // $table->foreignId('director_id')->nullable()->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
