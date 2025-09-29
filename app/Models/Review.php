ac83193e2cc46140322d2a494022d7964f888a5f
FTyumin <feodor.tjumin.28@gmail.com>
2025-09-29 16:21:03 +0300

    genre show page

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -22,7 +22,7 @@ class Review extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function movie() {
+    public function movies() {
         return $this->belongsTo(Movie::class);
     }
     
