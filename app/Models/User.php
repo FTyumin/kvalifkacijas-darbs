34dc1b9e685e3c5dc71941a6c1339198e0a4c546
FTyumin <feodor.tjumin.28@gmail.com>
2025-10-01 17:29:35 +0300

    change default email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\CustomVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -37,5 +38,10 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function reviews() {
         return $this->hasMany(Review::class);
+    }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new CustomVerifyEmail);
     }
 }
