094ef32f9b55e47f3e5c685b16dd689b630f134e
FTyumin <feodor.tjumin.28@gmail.com>
2025-10-04 17:01:41 +0300

    fix command

--- app/Console/Commands/InsertData.php
+++ app/Console/Commands/InsertData.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use App\Services\TmdbApiClient;
 
 class InsertData extends Command
@@ -20,12 +21,26 @@ class InsertData extends Command
      * @var string
      */
     protected $description = 'Command description';
-
+    protected $apiClient;
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        
+        $api = new TmdbApiClient;
+
+        $n = 100;
+        $data = $api->getTopMovies($n, ['method' => 'top-rated']);
+        $movies = collect($data ?? [])->map(function ($r) use ($api) {
+            return [
+                'name' => $r['title'] ?? null,
+                'year' => !empty($r['release_date']) ? substr($r['release_date'],0,4) : null,
+                'description' => $r['overview'],
+                'poster_url' => $r['poster_path'],
+            ];
+        })->all();
+
+        DB::table('movies')->insert($movies);
+
     }
 }
