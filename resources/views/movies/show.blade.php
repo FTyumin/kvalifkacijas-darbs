cf6a3bb8078b9b0841063bbd21a0e43e74f4a5c8
FTyumin <feodor.tjumin.28@gmail.com>
2025-12-31 13:08:42 +0200

    platform rating card on movie-show

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -15,10 +15,25 @@
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
         
         {{-- Poster --}}
-        <div class="md:col-span-1">
+        <div class="md:col-span-1 space-y-4">
             <img src="https://image.tmdb.org/t/p/w500/{{ $movie->poster_url }}" 
                  alt="{{ $movie->name }} poster" 
                  class="rounded-xl shadow-md w-full top-4">
+            @if($movie->rating)
+                <div class="rounded-xl border border-gray-700 bg-gray-800/60 p-4 shadow-md">
+                    <div class="flex items-center gap-3">
+                        <div class="flex h-10 w-10 items-center justify-center rounded-lg bg-yellow-500/10">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-5 w-5 text-yellow-500">
+                                <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div>
+                            <div class="text-xs uppercase tracking-wide text-gray-400">Platform Score</div>
+                            <div class="text-2xl font-semibold text-white">{{ $movie->rating }}</div>
+                        </div>
+                    </div>
+                </div>
+            @endif
         </div>
         {{-- Details --}}
         <div class="md:col-span-2 space-y-6">
@@ -32,10 +47,6 @@
                         <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
                     </svg>
                     <span class="text-sm text-white font-medium">{{ $movie->tmdb_rating }}</span>
-                    @if($movie->rating !== null)
-                        <span>•</span>
-                        <span class="text-sm text-white font-medium">{{ $movie->rating }}</span>
-                    @endif
                 </div>
                 <span>•</span>
                 <span class="text-sm uppercase">{{ $movie->language }}</span>
